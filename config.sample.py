02f6cc36ddef1e12ea5b93c728d6576439fc03bb
ibcht <irenee.bauchet@yahoo.fr>
2022-07-11 22:20:41 +0200

    change config path

---
+++ config.sample.py
@@ -0,0 +1,4 @@
+"""Use this file to override default settings"""
+
+DATABASE='tchou.sqlite' # path to the database instance
+SECRET_KEY='dev'
--- instance/config.sample.py
+++
@@ -1,4 +0,0 @@
-"""Use this file to override default settings"""
-
-# DATABASE='/path/to/tchou.sqlite'
-# SECRET_KEY='dev'
